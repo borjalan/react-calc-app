dc27d1bf62c8b8a201c9acec29c76d7586797934
Borja Landaburu <borja.landaburu@vipera.com>
2020-10-09 14:37:34 +0200

    Components Separation

---
+++ src/components/Functions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Functions = () => {
+  return (
+    <div className="functions">
+      <button>C</button>
+      <button>R</button>
+    </div>
+  );
+};
+
+export default Functions;
